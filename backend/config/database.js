fb60436c165add9d53827916b01805120bbdf001
diego <angelacardenasc@gmail.com>
2024-06-03 10:54:40 -0500

    commmit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-const uri = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://Morelus:1234@cluster0.tmaf4rl.mongodb.net/';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
