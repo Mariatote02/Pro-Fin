13e193451137c0f43fb0f97afd39a66f7a16c97b
Mariatote02 <tatiana.cubillos@hotmail.com>
2024-06-22 10:22:14 -0500

    "Added delete and update libro routes to backend, and corresponding service functions to frontend"

--- backend/controllers/libroController.js
+++ backend/controllers/libroController.js
@@ -2,15 +2,43 @@ const Libro = require("../models/libros");
 
 // Create new book
 const createLibro = async (req, res) => {
-  
+  try {
+    const libro = new Libro(req.body);
+    await libro.save();
+    res.status(201).send({ data: libro });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Error al crear libro' });
+  }
 }
 
-// Get all
+// Obtener todos
 const getAllLibros = async (req, res) => {
   const libros = await Libro.find();
   res.send({ data: libros });
 }
 
-// Update libro
+// Eliminar libro
+const deleteLibro = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Libro.findByIdAndDelete(id);
+    res.status(200).send({ message: 'Libro eliminado correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Error al eliminar libro' });
+  }
+}
 
-module.exports = { getAllLibros, createLibro }
+// Actualizar libro
+const updateLibro = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const libro = await Libro.findByIdAndUpdate(id, req.body, { new: true });
+    res.send({ data: libro });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Error al actualizar libro' });
+  }
+}
+module.exports = { createLibro, getAllLibros, deleteLibro, updateLibro }
